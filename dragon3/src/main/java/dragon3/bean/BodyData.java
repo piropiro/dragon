ef1e6df277ec0efd0a848760cccee2f64216ad58
piropiro <mela825@gmail.com>
2016-07-20 01:56:26 +0900

    lombok Dataクラス対応

--- dragon3/src/main/java/dragon3/bean/BodyData.java
+++ dragon3/src/main/java/dragon3/bean/BodyData.java
@@ -16,12 +16,13 @@ import dragon3.common.constant.WeponType;
 /**
  * @author k-saito
  */
+@lombok.Data
 public class BodyData implements Data {
 
-	private String id;
-	private String name;
-	private String image;
-	private BodyKind kind;
+	private String id = "none";
+	private String name = "none";
+	private String image = "0.png";
+	private BodyKind kind = BodyKind.CHARA;
 
 	private int hp;
 	private int str;
@@ -31,278 +32,18 @@ public class BodyData implements Data {
 	private int hit;
 	private int mis;
 	private int moveStep;
-	private MoveType moveType;
-	private SoulType soulType;
+	private MoveType moveType = MoveType.NONE;
+	private SoulType soulType = SoulType.NONE;
 
-	private WeponType weponType;
-	private ArmorType armorType;
+	private WeponType weponType = WeponType.NONE;
+	private ArmorType armorType = ArmorType.NONE;
 
-	private List<String> wazaList;
-	private List<BodyAttribute> attrList;
+	private List<String> wazaList = new ArrayList<>();
+	private List<BodyAttribute> attrList = new ArrayList<>();
 
-	public BodyData() {
-		id = "none";
-		name = "none";
-		image = "0.png";
-		kind = BodyKind.CHARA;
-		soulType = SoulType.NONE;
-		moveType = MoveType.NONE;
-		weponType = WeponType.NONE;
-		armorType = ArmorType.NONE;
-		wazaList = new ArrayList<>();
-		attrList = new ArrayList<>();
-	}
-
+	@Override
 	public String toString(){
 		return id + " - " + name;
-	}
-
-
-
-	/**
-	 * @return
-	 */
-	public ArmorType getArmorType() {
-		return armorType;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getDef() {
-		return def;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getHit() {
-		return hit;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getHp() {
-		return hp;
-	}
-
-	/**
-	 * @return
-	 */
-        @Override
-	public String getId() {
-		return id;
-	}
-
-	/**
-	 * @return
-	 */
-	public String getImage() {
-		return image;
-	}
-
-	/**
-	 * @return
-	 */
-	public BodyKind getKind() {
-		return kind;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getMdf() {
-		return mdf;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getMis() {
-		return mis;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getMoveStep() {
-		return moveStep;
-	}
-
-	/**
-	 * @return
-	 */
-	public MoveType getMoveType() {
-		return moveType;
-	}
-
-	/**
-	 * @return
-	 */
-	public int getMst() {
-		return mst;
-	}
-
-	/**
-	 * @return
-	 */
-        @Override
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @return
-	 */
-	public SoulType getSoulType() {
-		return soulType;
-	}
-
-
-	/**
-	 * @return
-	 */
-	public int getStr() {
-		return str;
-	}
-
-
-
-	public List<String> getWazaList() {
-		return wazaList;
-	}
-
-	public void setWazaList(List<String> wazaList) {
-		this.wazaList = wazaList;
-	}
-
-	public List<BodyAttribute> getAttrList() {
-		return attrList;
-	}
-
-	public void setAttrList(List<BodyAttribute> attrList) {
-		this.attrList = attrList;
-	}
-
-	/**
-	 * @return
-	 */
-	public WeponType getWeponType() {
-		return weponType;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setArmorType(ArmorType string) {
-		armorType = string;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setDef(int i) {
-		def = i;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setHit(int i) {
-		hit = i;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setHp(int i) {
-		hp = i;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setId(String string) {
-		id = string;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setImage(String string) {
-		image = string;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setKind(BodyKind string) {
-		kind = string;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setMdf(int i) {
-		mdf = i;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setMis(int i) {
-		mis = i;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setMoveStep(int i) {
-		moveStep = i;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setMoveType(MoveType string) {
-		moveType = string;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setMst(int i) {
-		mst = i;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setName(String string) {
-		name = string;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setSoulType(SoulType string) {
-		soulType = string;
-	}
-
-	/**
-	 * @param i
-	 */
-	public void setStr(int i) {
-		str = i;
-	}
-
-
-	/**
-	 * @param string
-	 */
-	public void setWeponType(WeponType string) {
-		weponType = string;
 	}
 
 }
--- dragon3/src/main/java/dragon3/bean/SaveData.java
+++ dragon3/src/main/java/dragon3/bean/SaveData.java
@@ -2,6 +2,7 @@ package dragon3.bean;
 
 import java.io.Serializable;
 
+@lombok.Data
 public class SaveData implements Serializable, Cloneable {
 
 	private static final long serialVersionUID = 761813354588427943L;
@@ -21,22 +22,6 @@ public class SaveData implements Serializable, Cloneable {
 	private long playTime;
 	private String playerName;
 
-	public SaveData() {
-		reverse = false;
-		allClear = false;
-		playerName = null;
-		score = 0;
-		mapNum = 0;
-		enemyLevel = 0;
-		turn = 0;
-		dead = 0;
-		kill = 0;
-		item = 0;
-		escape = 0;
-		save = 0;
-		stage = 0;
-		playTime = 0;
-	}
 
 	public SaveData copy() {
 		try {
@@ -70,174 +55,5 @@ public class SaveData implements Serializable, Cloneable {
 	}
 	public void addTime(long n) {
 		playTime += n;
-	}
-
-	/**
-	 * @return Returns the allClear.
-	 */
-	public boolean isAllClear() {
-		return allClear;
-	}
-	/**
-	 * @param allClear The allClear to set.
-	 */
-	public void setAllClear(boolean allClear) {
-		this.allClear = allClear;
-	}
-	/**
-	 * @return Returns the dead.
-	 */
-	public int getDead() {
-		return dead;
-	}
-	/**
-	 * @param dead The dead to set.
-	 */
-	public void setDead(int dead) {
-		this.dead = dead;
-	}
-	/**
-	 * @return Returns the enemyLevel.
-	 */
-	public int getEnemyLevel() {
-		return enemyLevel;
-	}
-	/**
-	 * @param enemyLevel The enemyLevel to set.
-	 */
-	public void setEnemyLevel(int enemyLevel) {
-		this.enemyLevel = enemyLevel;
-	}
-	/**
-	 * @return Returns the escape.
-	 */
-	public int getEscape() {
-		return escape;
-	}
-	/**
-	 * @param escape The escape to set.
-	 */
-	public void setEscape(int escape) {
-		this.escape = escape;
-	}
-	/**
-	 * @return Returns the item.
-	 */
-	public int getItem() {
-		return item;
-	}
-	/**
-	 * @param item The item to set.
-	 */
-	public void setItem(int item) {
-		this.item = item;
-	}
-	/**
-	 * @return Returns the kill.
-	 */
-	public int getKill() {
-		return kill;
-	}
-	/**
-	 * @param kill The kill to set.
-	 */
-	public void setKill(int kill) {
-		this.kill = kill;
-	}
-	/**
-	 * @return Returns the mapNum.
-	 */
-	public int getMapNum() {
-		return mapNum;
-	}
-	/**
-	 * @param mapNum The mapNum to set.
-	 */
-	public void setMapNum(int mapNum) {
-		this.mapNum = mapNum;
-	}
-	/**
-	 * @return Returns the playerName.
-	 */
-	public String getPlayerName() {
-		return playerName;
-	}
-	/**
-	 * @param playerName The playerName to set.
-	 */
-	public void setPlayerName(String playerName) {
-		this.playerName = playerName;
-	}
-	/**
-	 * @return Returns the playTyme.
-	 */
-	public long getPlayTime() {
-		return playTime;
-	}
-	/**
-	 * @param playTyme The playTyme to set.
-	 */
-	public void setPlayTime(long playTyme) {
-		this.playTime = playTyme;
-	}
-	/**
-	 * @return Returns the reverse.
-	 */
-	public boolean isReverse() {
-		return reverse;
-	}
-	/**
-	 * @param reverse The reverse to set.
-	 */
-	public void setReverse(boolean reverse) {
-		this.reverse = reverse;
-	}
-	/**
-	 * @return Returns the save.
-	 */
-	public int getSave() {
-		return save;
-	}
-	/**
-	 * @param save The save to set.
-	 */
-	public void setSave(int save) {
-		this.save = save;
-	}
-	/**
-	 * @return Returns the score.
-	 */
-	public int getScore() {
-		return score;
-	}
-	/**
-	 * @param score The score to set.
-	 */
-	public void setScore(int score) {
-		this.score = score;
-	}
-	/**
-	 * @return Returns the stage.
-	 */
-	public int getStage() {
-		return stage;
-	}
-	/**
-	 * @param stage The stage to set.
-	 */
-	public void setStage(int stage) {
-		this.stage = stage;
-	}
-	/**
-	 * @return Returns the turn.
-	 */
-	public int getTurn() {
-		return turn;
-	}
-	/**
-	 * @param turn The turn to set.
-	 */
-	public void setTurn(int turn) {
-		this.turn = turn;
 	}
 }
